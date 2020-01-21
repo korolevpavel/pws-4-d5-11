0d2538b6c305d522f2557a2887483c1174b1b800
Korolev Pavel <korolev.vrn@gmail.com>
2020-01-21 23:19:44 +0300

    update

--- my_site/settings.py
+++ my_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0', 'protected-harbor-42323.herokuapp.com']
 
 
 # Application definition
